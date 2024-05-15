fbe43aceefc2214a0af40b323d3074aff176fd64
Rogerio-17 <rogeriojmf10@gmail.com>
2024-05-15 19:10:03 -0300

    teste

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import { fonts } from '@/styles/themes/fonts'
-import Header from '@/components/Header'
-import { Footer } from '@/components/Footer'
 import { Providers } from './providers'
 import Head from 'next/head'
+import Header from '@/components/Header'
+import { Footer } from '@/components/Footer'
 
 export default function RootLayout({
     children,
